87b88adf42d185f051d7ae01b5e2e0a3b81fc0ee
Axelszetu <76113789+Axelszetu@users.noreply.github.com>
2022-06-09 18:56:56 +0200

    Added SimOU that creates a process of dimensions d x N, in accordance with the setup of the article.w

--- Main Functions.R
+++ Main Functions.R
@@ -1,2 +1,23 @@
 # Core functions
 # Initial commit to test if the project is connected to GitHub
+
+SimOU <- function(d = 1, A0 = diag(d), t = 10, N = 1000){
+  #We need a d-dmiensional brownian motion.
+  samples = rnorm(n = d*N, mean = 0, sd = sqrt(t/N))
+  increment_matrix <- matrix(data = samples, nrow = d, ncol = N, byrow = T)
+  #Create matrix to contain OU process
+  OU <- matrix(nrow = d, ncol = N+1)
+  #Initializing
+  OU[,1] <- numeric(d)
+  #Computation of reusable constant
+  reversion_factor <- diag(d)-A0*(t/N)
+  #Computation of OU paths
+  for (i in 2:(N+1)){
+    OU[,i] <- reversion_factor%*%OU[,i-1] + increment_matrix[,i-1]
+  }
+  OU
+}
+
+OU_MLE_analytical <- function(X, dt){
+  dX <- apply(X, MARGIN = 1, FUN = diff)
+}
