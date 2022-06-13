55dd8ac0760f6db8804bd686be8260e47c28286a
Axelszetu <76113789+Axelszetu@users.noreply.github.com>
2022-06-13 14:03:41 +0200

    Working implementation of numeric MLE using the trace computation for much improved efficiency. Tested on 2x2 drift matrix.

--- Main Functions.R
+++ Main Functions.R
@@ -122,3 +122,39 @@ OU_Lasso <- function(X, dt, lambda){
   optimal_pars_matrix <- matrix(data = optimal_pars_vector, nrow = d, ncol = d)
   optimal_pars_matrix
 }
+
+likelihood_evaluator_trace <- function(A, B, C){
+  d <- nrow(B)
+  A <- matrix(data = A, nrow = d, ncol = d)
+  
+  likelihood <- sum(diag(B%*%t(A))) + sum(diag(A%*%C%*%t(A)))
+  likelihood
+}
+
+OU_MLE_numeric_trace <- function(X, dt){
+  d <- nrow(X)
+  N <- ncol(X)
+  dX <- apply(X, MARGIN = 1, FUN = diff)
+  dX <- t(dX)
+  
+  B <- matrix(data = 0, nrow = d, ncol = d)
+  for (i in (1:(N-1))){
+    M <- dX[,i]%*%t(X[,i])
+    B <- B + M
+  }
+  B <- B/(N-1)
+  
+  C <- matrix(data = 0, nrow = d, ncol = d)
+  for (i in (1:N)){
+    M <- X[,i]%*%t(X[,i])
+    C <- C + M
+  }
+  C <- C/(2*N)
+  C <- C*dt
+  
+  par <- diag(d)
+  par <- as.vector(par)
+  optimal_pars_vector <- optim(par = par, fn = likelihood_evaluator_trace, B = B, C = C)$par
+  optimal_pars_matrix <- matrix(data = optimal_pars_vector, nrow = d, ncol = d)
+  optimal_pars_matrix
+}
